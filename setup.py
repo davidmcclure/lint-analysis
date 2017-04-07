9767687d3f92e2b2d74596296362052297d75367
David McClure <davidwilliammcclure@gmail.com>
2017-04-07 16:39:01 -0700

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,15 @@
+
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='lint-analysis',
+    version='0.1.0',
+    description='How do words distribute inside of texts?',
+    url='https://github.com/davidmcclure/lint-analysis',
+    license='MIT',
+    author='David McClure',
+    author_email='dclure@stanford.edu',
+    packages=find_packages(),
+)
