4e0faf31b793b8f48f59f7212adf2e879ed11c0c
David McClure <davidwilliammcclure@gmail.com>
2017-04-08 22:49:14 -0700

    Ready for ingest.

--- bin_counts/models.py
+++ bin_counts/models.py
@@ -2,10 +2,13 @@
 
 import ujson
 
+from datetime import datetime as dt
+
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, PrimaryKeyConstraint
+from sqlalchemy.schema import Index
 
-from bin_counts.db import session
+from bin_counts.db import session, engine
 from bin_counts.utils import scan_paths
 
 
@@ -14,19 +17,13 @@ Base = declarative_base()
 Base.query = session.query_property()
 
 
-class Bucket(Base):
+class BinCount(Base):
 
-    __tablename__ = 'bucket'
+    __tablename__ = 'bin_count'
 
-    __table_args__ = (
-        PrimaryKeyConstraint(
-            'corpus',
-            'year',
-            'token',
-            'pos',
-            'bin',
-        ),
-    )
+    __table_args__ = dict(sqlite_autoincrement=True)
+
+    id = Column(Integer, primary_key=True)
 
     corpus = Column(String, nullable=False)
 
@@ -51,4 +48,40 @@ class Bucket(Base):
                 session.bulk_insert_mappings(cls, segment)
 
                 session.commit()
-                print(path)
+                print(dt.now(), path)
+
+    # TODO: Move to base class.
+    @classmethod
+    def add_index(cls, *cols, **kwargs):
+        """Add an index to the table.
+        """
+        # Make slug from column names.
+        col_names = '_'.join([c.name for c in cols])
+
+        # Build the index name.
+        name = 'idx_{}_{}'.format(cls.__tablename__, col_names)
+
+        idx = Index(name, *cols, **kwargs)
+
+        # Render the index.
+        idx.create(bind=engine)
+
+    @classmethod
+    def add_indexes(cls):
+        """Add indexes.
+        """
+        # Query perf.
+        cls.add_index(cls.corpus)
+        cls.add_index(cls.year)
+        cls.add_index(cls.token)
+        cls.add_index(cls.pos)
+
+        # Ensure unique.
+        cls.add_index(
+            cls.corpus,
+            cls.year,
+            cls.token,
+            cls.pos,
+            cls.bin,
+            unique=True,
+        )
--- tasks.py
+++ tasks.py
@@ -3,11 +3,19 @@
 from invoke import task
 
 from bin_counts.db import engine
-from bin_counts.models import Base
+from bin_counts.models import Base, BinCount
 
 
 @task
-def init_bin_counts_db(ctx):
-    """Create the bin-counts database.
+def reset_bin_counts_db(ctx):
+    """Recreate the bin counts database.
     """
+    Base.metadata.drop_all(engine)
     Base.metadata.create_all(engine)
+
+
+@task
+def index_bin_counts_db(ctx):
+    """Create indexes on bin counts database.
+    """
+    BinCount.add_indexes()
