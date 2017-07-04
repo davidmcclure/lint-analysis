cc5f29d007d98b1f563c042ed5a5550aa1f3a231
David McClure <davidwilliammcclure@gmail.com>
2017-07-04 10:39:03 -0700

    Fix cache module.

---
+++ lint_analysis/bin_counts/cache.py
@@ -0,0 +1,15 @@
+
+
+import os
+
+from joblib import Memory
+
+from lint_analysis.bin_counts.models import BinCount
+
+
+cache_dir = os.path.join(os.path.dirname(__file__), 'cache')
+memory = Memory(cache_dir)
+
+
+token_counts = memory.cache(BinCount.token_counts)
+token_series = memory.cache(BinCount.token_series)
