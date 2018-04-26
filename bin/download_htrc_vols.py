00d119483d7c62d5161b14d8a4f2f73fdea1a2ff
David McClure <davidwilliammcclure@gmail.com>
2018-04-26 11:09:02 -0400

    HTRC download bin.

---
+++ bin/download_htrc_vols.py
@@ -0,0 +1,24 @@
+
+
+import click
+import pandas as pd
+
+from tqdm import tqdm
+from htrc_features.utils import download_file
+from boltons.iterutils import chunked
+
+
+@click.command()
+@click.argument('df_path', type=click.Path())
+@click.argument('out_dir', type=click.Path())
+def download(df_path, out_dir):
+    """Download HTRC features vols.
+    """
+    df = pd.read_json(df_path)
+
+    for htids in tqdm(chunked(list(df.htid), 1000)):
+        download_file(htids, outdir=out_dir)
+
+
+if __name__ == '__main__':
+    download()
